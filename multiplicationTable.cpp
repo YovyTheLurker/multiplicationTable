3f49296266e4f6b42b9da68fa1feb03cdadfee37
Yoevelyn Rodriguez <yoerodri@yahoo.com>
2021-06-13 18:13:47 -0400

    Add files via upload

---
+++ multiplicationTable.cpp
@@ -0,0 +1,17 @@
+// multiplicationTable.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//Outer and inner for loops.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <= 10; i++) {
+		for (int j = 1; j <= 10; j++) {
+			cout << i << " * " << j << " = " << i * j << endl;
+		}
+		cout << endl;
+	}
+}
+
+
